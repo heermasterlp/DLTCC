9f9ebd4d257abb5dd15fc2050a1c28e00eaf0a83
刘鹏 <liupeng@liupengs-MacBook-Pro.local>
2017-03-25 01:31:39 +0800

    train 100x100

--- DLTCC/dltcc_simple.py
+++ DLTCC/dltcc_simple.py
@@ -91,7 +91,9 @@ def train():
     today = "{}-{}-{}".format(now.year, now.month, now.day)
 
     # Train models
-    with tf.Session() as sess:
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    with tf.Session(config=config) as sess:
         sess.run(init_op)
 
         # Train the models
