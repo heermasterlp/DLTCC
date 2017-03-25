6c8842923fd4d2c6642b3a3e61cccc78cf270d46
刘鹏 <liupeng@liupengs-MacBook-Pro.local>
2017-03-25 10:04:02 +0800

    fix models

--- DLTCC/dltcc_models.py
+++ DLTCC/dltcc_models.py
@@ -7,7 +7,6 @@ SIZE = 100
 IMAGE_WIDTH = SIZE
 IMAGE_HEIGHT = SIZE
 
-
 class Dltcc(object):
     def __init__(self):
         pass
@@ -21,17 +20,17 @@ class Dltcc(object):
 
         # Conv 1
         with tf.name_scope("conv1"):
-            self.conv1 = conv_layer(input=self.x_reshape, input_channels=1, filter_size=3, output_channels=16, use_pooling=True)
+            self.conv1 = conv_layer(input=self.x_reshape, input_channels=1, filter_size=7, output_channels=4, use_pooling=True)
 
         with tf.name_scope("conv2"):
-            self.conv2 = conv_layer(input=self.conv1, input_channels=16, filter_size=3, output_channels=16, use_pooling=True)
+            self.conv2 = conv_layer(input=self.conv1, input_channels=4, filter_size=5, output_channels=8, use_pooling=True)
             # Conv 3
         with tf.name_scope("conv3"):
-            self.conv3 = conv_layer(input=self.conv2, input_channels=16, filter_size=3, output_channels=32, use_pooling=True)
+            self.conv3 = conv_layer(input=self.conv2, input_channels=8, filter_size=3, output_channels=16, use_pooling=True)
 
             # Conv 4
         with tf.name_scope("conv4"):
-            self.conv4 = conv_layer(input=self.conv3, input_channels=32, filter_size=3, output_channels=32, use_pooling=True)
+            self.conv4 = conv_layer(input=self.conv3, input_channels=16, filter_size=3, output_channels=32, use_pooling=True)
 
             # Flatten layer
         with tf.name_scope("flatten1"):
@@ -50,6 +49,50 @@ class Dltcc(object):
             self.y_prob = tf.sigmoid(self.layer_fc2)
 
             print("Build models end!")
+
+
+# class Dltcc(object):
+#     def __init__(self):
+#         pass
+#
+#     # Build the models
+#     def build(self, inputs):
+#         if inputs is None:
+#             print("Input should not none!")
+#
+#         self.x_reshape = tf.reshape(inputs, [-1, IMAGE_WIDTH, IMAGE_HEIGHT, 1], name="x_reshape")
+#
+#         # Conv 1
+#         with tf.name_scope("conv1"):
+#             self.conv1 = conv_layer(input=self.x_reshape, input_channels=1, filter_size=3, output_channels=16, use_pooling=True)
+#
+#         with tf.name_scope("conv2"):
+#             self.conv2 = conv_layer(input=self.conv1, input_channels=16, filter_size=3, output_channels=16, use_pooling=True)
+#             # Conv 3
+#         with tf.name_scope("conv3"):
+#             self.conv3 = conv_layer(input=self.conv2, input_channels=16, filter_size=3, output_channels=32, use_pooling=True)
+#
+#             # Conv 4
+#         with tf.name_scope("conv4"):
+#             self.conv4 = conv_layer(input=self.conv3, input_channels=32, filter_size=3, output_channels=32, use_pooling=True)
+#
+#             # Flatten layer
+#         with tf.name_scope("flatten1"):
+#             self.layer_flat, self.num_flat_features = flatten_layer(self.conv4)
+#
+#         with tf.name_scope("fc_layer"):
+#             self.layer_fc1 = new_fc_layer(input=self.layer_flat, num_inputs=self.num_flat_features,
+#                                      num_outputs=IMAGE_WIDTH * IMAGE_HEIGHT * 2, use_sigmoid=True)
+#
+#             self.layer_fc2 = new_fc_layer(input=self.layer_fc1, num_inputs=IMAGE_WIDTH * IMAGE_HEIGHT * 2,
+#                                      num_outputs=IMAGE_WIDTH * IMAGE_HEIGHT, use_sigmoid=True)
+#
+#             # Predict
+#         with tf.name_scope("probability"):
+#             # layer_dropped = tf.nn.dropout(layer_fc2, keep_prob=1.0)
+#             self.y_prob = tf.sigmoid(self.layer_fc2)
+#
+#             print("Build models end!")
 
 
 # Create a new Convolution layer
