301bc29a94ab773e500ad52639ef71c1eda7ab02
刘鹏 <liupeng@liupengs-MacBook-Pro.local>
2017-03-25 18:58:21 +0800

    add heng train models

---
+++ DLTCC/dltcc_heng.py
@@ -0,0 +1,288 @@
+from __future__ import absolute_import
+
+import datetime
+import os
+
+import tensorflow as tf
+
+import input_data
+
+# 200x40 data set
+train_data_dir = "../../DataSet/DataSetFiles/TrainSet/Kai_heng_200_40_30_train.npy"
+train_target_dir = "../../DataSet/DataSetFiles/TrainSet/Qigong_heng_200_40_30_train.npy"
+
+test_data_dir = "../../DataSet/DataSetFiles/TestSet/Kai_heng_200_40_11_test.npy"
+test_target_dir = "../../DataSet/DataSetFiles/TestSet/Qigong_heng_200_40_11_test.npy"
+
+# train_data_dir = "../../DataSet/DataSetFiles/TrainSet/Kai_100_100_200_train.npy"
+# train_target_dir = "../../DataSet/DataSetFiles/TrainSet/Qigong_100_100_200_train.npy"
+#
+# test_data_dir = "../../DataSet/DataSetFiles/TestSet/Kai_100_100_20_test.npy"
+# test_target_dir = "../../DataSet/DataSetFiles/TestSet/Qigong_100_100_20_test.npy"
+
+# validation size
+VALIDATION_SIZE = 50
+
+# train data set files
+train_dir = {"train": {"data": train_data_dir, "target": train_target_dir},
+             "test": {"data": test_data_dir, "target": test_target_dir}}
+
+
+IMAGE_WIDTH = 200
+IMAGE_HEIGHT = 40
+
+model_path = "../../checkpoints/models_200_40_"
+checkpoint_path = "../../checkpoints/checkpoints_200_40"
+
+# threshold
+THEROSHOLD = 0.7
+
+# max training epoch
+MAX_TRAIN_EPOCH = 10000
+
+
+def train():
+    # Data set
+    data_set = input_data.read_data_sets(train_dir, validation_size=2)
+
+    if data_set is None:
+        print("data sets are none!")
+        return
+    else:
+        print("data set train data shape:{}".format(data_set.train.data.shape))
+        print("data set train target shape:{}".format(data_set.train.target.shape))
+        print("data set test data shape:{}".format(data_set.test.data.shape))
+        print("data set test target shape:{}".format(data_set.test.target.shape))
+
+    print("Start build models")
+
+    # place variable
+    x = tf.placeholder(tf.float32, shape=[None, IMAGE_WIDTH * IMAGE_HEIGHT], name="x")
+    y_true = tf.placeholder(tf.float32, shape=[None, IMAGE_WIDTH * IMAGE_HEIGHT], name="y_true")
+
+    dltcc_obj = DltccHeng()
+    dltcc_obj.build(x)
+
+    # Loss
+    with tf.device("cpu:0"):
+        cost_op = tf.reduce_mean((y_true - dltcc_obj.y_prob) ** 2)
+        optimizer_op = tf.train.RMSPropOptimizer(0.01).minimize(cost_op)
+
+    print("Build models end!")
+
+    # initialize variable
+    init_op = tf.global_variables_initializer()
+
+    # save the models and checkpoints. the formatting: (models) models-date.ckpt, (checkpoint) checkpoint-date-step.ckpt
+    saver = tf.train.Saver()
+
+    if not os.path.exists(model_path):
+        os.mkdir(model_path)
+    if not os.path.exists(checkpoint_path):
+        os.mkdir(checkpoint_path)
+
+    now = datetime.datetime.now()
+    today = "{}-{}-{}".format(now.year, now.month, now.day)
+
+    # Train models
+    # config = tf.ConfigProto()
+    # config.gpu_options.allow_growth = True
+    with tf.Session() as sess:
+        sess.run(init_op)
+
+        # Train the models
+        for epoch in range(MAX_TRAIN_EPOCH):
+            x_batch = data_set.train.data
+            y_batch = data_set.train.target
+
+            _, cost = sess.run([optimizer_op, cost_op], feed_dict={x: x_batch, y_true: y_batch})
+
+            if epoch % 100 == 0:
+                print("Epoch {0} : {1}".format(epoch, cost))
+
+        # Save the trained models.
+        saver.save(sess, os.path.join(model_path, "models-{}".format(today)))
+        print("Training end!")
+
+
+def evaluate():
+    pass
+
+
+def inference():
+    pass
+
+
+class DltccHeng(object):
+    def __init__(self):
+        pass
+
+    # Build the models
+    def build(self, inputs):
+        if inputs is None:
+            print("Input should not none!")
+
+        self.x_reshape = tf.reshape(inputs, [-1, IMAGE_WIDTH, IMAGE_HEIGHT, 1], name="x_reshape")
+
+        # Conv 1
+        with tf.name_scope("conv1"):
+            self.conv1 = conv_layer(input=self.x_reshape, input_channels=1, filter_size=3, output_channels=4, use_pooling=True)
+
+        with tf.name_scope("conv2"):
+            self.conv2 = conv_layer(input=self.conv1, input_channels=4, filter_size=3, output_channels=8, use_pooling=True)
+            # Conv 3
+        with tf.name_scope("conv3"):
+            self.conv3 = conv_layer(input=self.conv2, input_channels=8, filter_size=3, output_channels=16, use_pooling=True)
+
+            # Conv 4
+        with tf.name_scope("conv4"):
+            self.conv4 = conv_layer(input=self.conv3, input_channels=16, filter_size=3, output_channels=16, use_pooling=True)
+
+            # Flatten layer
+        with tf.name_scope("flatten1"):
+            self.layer_flat, self.num_flat_features = flatten_layer(self.conv4)
+
+        with tf.name_scope("fc_layer"):
+
+            self.layer_fc2 = new_fc_layer(input=self.layer_flat, num_inputs=self.num_flat_features,
+                                     num_outputs=IMAGE_WIDTH * IMAGE_HEIGHT, use_sigmoid=True)
+
+            # Predict
+        with tf.name_scope("probability"):
+            # layer_dropped = tf.nn.dropout(layer_fc2, keep_prob=1.0)
+            self.y_prob = tf.sigmoid(self.layer_fc2)
+
+
+# Create a new Convolution layer
+def conv_layer(input,  # The previous layer.
+                   input_channels,  # Num. channels in prev. layer.
+                   filter_size,  # Width and height of each filter.
+                   output_channels,  # Number of filters.
+                   use_pooling=True):  # Use 2x2 max-pooling.
+
+    # Shape of the filter-weights for the convolution.
+    # This format is determined by the TensorFlow API.
+    shape = [filter_size, filter_size, input_channels, output_channels]
+
+    # Create new weights aka. filters with the given shape.
+    weights = new_weights(shape=shape)
+
+    # Create new biases, one for each filter.
+    biases = new_biases(length=output_channels)
+
+    # Create the TensorFlow operation for convolution.
+    # Note the strides are set to 1 in all dimensions.
+    # The first and last stride must always be 1,
+    # because the first is for the image-number and
+    # the last is for the input-channel.
+    # But e.g. strides=[1, 2, 2, 1] would mean that the filter
+    # is moved 2 pixels across the x- and y-axis of the image.
+    # The padding is set to 'SAME' which means the input image
+    # is padded with zeroes so the size of the output is the same.
+    # layer = tf.nn.conv2d(input=input,
+    #                      filter=weights,
+    #                      strides=[1, 1, 1, 1],
+    #                      padding='SAME')
+    layer = tf.nn.conv2d(input=input,
+                         filter=weights,
+                         strides=[1, 1, 1, 1],
+                         padding="SAME")
+
+    # Add the biases to the results of the convolution.
+    # A bias-value is added to each filter-channel.
+    layer += biases
+
+    # Use pooling to down-sample the image resolution?
+    if use_pooling:
+        # This is 2x2 max-pooling, which means that we
+        # consider 2x2 windows and select the largest value
+        # in each window. Then we move 2 pixels to the next window.
+        # layer = tf.nn.max_pool(value=layer,
+        #                        ksize=[1, 2, 2, 1],
+        #                        strides=[1, 2, 2, 1],
+        #                        padding='SAME')
+        layer = tf.nn.max_pool(value=layer,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding="SAME")
+
+    # Rectified Linear Unit (ReLU).
+    # It calculates max(x, 0) for each input pixel x.
+    # This adds some non-linearity to the formula and allows us
+    # to learn more complicated functions.
+    layer = tf.nn.relu(layer)
+
+    # Note that ReLU is normally executed before the pooling,
+    # but since relu(max_pool(x)) == max_pool(relu(x)) we can
+    # save 75% of the relu-operations by max-pooling first.
+
+    # We return both the resulting layer and the filter-weights
+    # because we will plot the weights later.
+    return layer
+
+
+def avg_pool(inputs, name):
+    return tf.nn.avg_pool(inputs, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID', name=name)
+
+
+def max_pool(inputs, name):
+    return tf.nn.max_pool(inputs, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID', name=name)
+
+
+# flattening a layer
+def flatten_layer(layer):
+    # Get the shape of the input layer.
+    layer_shape = layer.get_shape()
+
+    # The shape of the input layer is assumed to be:
+    # layer_shape == [num_images, img_height, img_width, num_channels]
+
+    # The number of features is: img_height * img_width * num_channels
+    # We can use a function from TensorFlow to calculate this.
+    num_features = layer_shape[1:4].num_elements()
+
+    # Reshape the layer to [num_images, num_features].
+    # Note that we just set the size of the second dimension
+    # to num_features and the size of the first dimension to -1
+    # which means the size in that dimension is calculated
+    # so the total size of the tensor is unchanged from the reshaping.
+    layer_flat = tf.reshape(layer, [-1, num_features])
+
+    # The shape of the flattened layer is now:
+    # [num_images, img_height * img_width * num_channels]
+
+    # Return both the flattened layer and the number of features.
+    return layer_flat, num_features
+
+
+# Fully connected layer
+def new_fc_layer(input,          # The previous layer.
+                 num_inputs,     # Num. inputs from prev. layer.
+                 num_outputs,    # Num. outputs.
+                 use_sigmoid=True):  # Use Rectified Linear Unit (ReLU)?
+
+    # Create new weights and biases.
+    weights = new_weights(shape=[num_inputs, num_outputs])
+    biases = new_biases(length=num_outputs)
+
+    # Calculate the layer as the matrix multiplication of
+    # the input and weights, and then add the bias-values.
+    layer = tf.matmul(input, weights) + biases
+
+    # Use ReLU?
+    if use_sigmoid:
+        layer = tf.nn.sigmoid(layer)
+
+    return layer
+
+
+# create new variables
+def new_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+
+
+def new_biases(length):
+    return tf.Variable(tf.constant(0.05, shape=[length]))
+
+if __name__ == "__main__":
+    train()
