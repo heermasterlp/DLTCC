7866ee38030308648e3704030d3d9b0e85496679
刘鹏 <liupeng@liupengs-MacBook-Pro.local>
2017-04-02 18:56:56 +0800

    fix characters models

--- dltcc_characters/models.py
+++ dltcc_characters/models.py
@@ -17,7 +17,7 @@ class Dltcc(object):
         # Conv 1
         with tf.name_scope("conv1"):
             self.conv1 = conv_layer(input=self.x_reshape, input_channels=1, filter_size=3, output_channels=5,
-                                    use_pooling=False, phase_train=phase_train)
+                                    use_pooling=True, phase_train=phase_train)
 
         with tf.name_scope("conv2"):
             self.conv2 = conv_layer(input=self.conv1, input_channels=5, filter_size=3, output_channels=10,
@@ -25,7 +25,7 @@ class Dltcc(object):
             # Conv 3
         with tf.name_scope("conv3"):
             self.conv3 = conv_layer(input=self.conv2, input_channels=10, filter_size=3, output_channels=20,
-                                    use_pooling=False, phase_train=phase_train)
+                                    use_pooling=True, phase_train=phase_train)
 
             # Conv 4
         with tf.name_scope("conv4"):
