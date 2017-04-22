784d78cb61f0dfcf288e1f917e8c1656a0b365ea
刘鹏 <liupeng@liupengs-MacBook-Pro.local>
2017-04-23 00:01:39 +0800

    fix bug

--- dltcc_heng_cnn/models.py
+++ dltcc_heng_cnn/models.py
@@ -50,19 +50,19 @@ class DltccHeng(object):
             # [batch, 100, 20, ngf] => [batch, 200, 40, 1]
             self.deconv1 = deconv2d(self.de_rectified2, out_channels=1, name="deconv1")
             self.de_batchnorm1 = batchnorm(self.deconv1, name="de_batchnorm1")
-            self.deconv_out = tf.nn.relu(self.de_batchnorm1, name="out")
+            self.out = tf.nn.sigmoid(self.de_batchnorm1, name="out")
 
             # FC layers
-            self.layer_flat, self.num_flat_features = flatten_layer(self.deconv_out)
-            # FC layer 1
-            self.layer_fc1 = new_fc_layer(self.layer_flat, num_inputs=self.num_flat_features,
-                                          num_outputs=1000)
-
-            # self.layer_fc2 = new_fc_layer(self.layer_fc1, num_inputs=1000, num_outputs=1000)
-            # self.layer_fc3 = new_fc_layer(self.layer_fc2, num_inputs=1000, num_outputs=1000)
-
-            self.layer_fc2 = new_fc_layer(self.layer_fc1, num_inputs=1000,
-                                          num_outputs=self.image_width*self.image_height)
-            # out
-            self.out = tf.nn.sigmoid(self.layer_fc2)
+            # self.layer_flat, self.num_flat_features = flatten_layer(self.deconv_out)
+            # # FC layer 1
+            # self.layer_fc1 = new_fc_layer(self.layer_flat, num_inputs=self.num_flat_features,
+            #                               num_outputs=1000)
+            #
+            # # self.layer_fc2 = new_fc_layer(self.layer_fc1, num_inputs=1000, num_outputs=1000)
+            # # self.layer_fc3 = new_fc_layer(self.layer_fc2, num_inputs=1000, num_outputs=1000)
+            #
+            # self.layer_fc2 = new_fc_layer(self.layer_fc1, num_inputs=1000,
+            #                               num_outputs=self.image_width*self.image_height)
+            # # out
+            # self.out = tf.nn.sigmoid(self.layer_fc2)
 
